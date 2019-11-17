70f1d548acb708d1ab320c7874afb01c630a2c4a
AYassine <assiryassine@gmail.com>
2019-11-17 11:25:10 +0100

    Show resource life cycle (singleton and per-request resources)

--- src/main/java/com/assir/yassine/rest/MyResource.java
+++ src/main/java/com/assir/yassine/rest/MyResource.java
@@ -1,17 +1,22 @@
 package com.assir.yassine.rest;
 
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("test")
+@Singleton
 public class MyResource {
+
+    private int count;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String testMethod() {
-        return "Test";
+        count += 1;
+        return "Test works! This method was called " + count + " time(s)";
     }
 }
