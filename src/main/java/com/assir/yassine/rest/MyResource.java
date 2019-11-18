6f83a445ef024b86d147a5ee75b28e8c1861922d
AYassine <assiryassine@gmail.com>
2019-11-18 18:21:10 +0100

    Implement a MessageBodyWriter for Date type

--- src/main/java/com/assir/yassine/rest/MyResource.java
+++ src/main/java/com/assir/yassine/rest/MyResource.java
@@ -3,23 +3,15 @@ package com.assir.yassine.rest;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Calendar;
+import java.util.Date;
 
-@Path("{pathParam}/test")
+@Path("test")
 public class MyResource {
-
-    /*
-    With @Singleton resource, this does not work.
-    Because, singleton resources are instantiated during application startup.
-    So, you can not inject request-specific information to their member variables.
-     */
-
-    @PathParam("pathParam") private String pathParamExample;
-    @QueryParam("query") private String queryParamExample;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String testMethod() {
-        return "Test works! Path param used: " + pathParamExample +
-                " and Query param used: " + queryParamExample;
+    public Date testMethod() {
+        return Calendar.getInstance().getTime();
     }
 }
